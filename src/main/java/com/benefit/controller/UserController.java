3994f0857866dc874168f1ac479224e80ab425f8
LoserDai <15570237906@139.com>
2025-06-06 16:32:20 +0800

    add user interface

---
+++ src/main/java/com/benefit/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.benefit.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.benefit.common.BaseResponse;
+import com.benefit.common.ResultUtils;
+import com.benefit.exception.BusinessException;
+import com.benefit.model.entity.User;
+import com.benefit.model.enums.ErrorCode;
+import com.benefit.request.UserRegisterRequest;
+import com.benefit.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+
+/**
+ * @author Allen
+ * @date 2025/6/6 14:53
+ */
+@RestController
+@RequestMapping("/user")
+//支持跨域请求后端
+@CrossOrigin(origins = {"http://localhost:3000"})
+@Slf4j
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+
+    @PostMapping("/register")
+    public BaseResponse<Long> userRegister(@RequestBody UserRegisterRequest userRegisterRequest) {
+        //校验
+        if (userRegisterRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+
+        String userAccount = userRegisterRequest.getUserAccount();
+        String userPassword = userRegisterRequest.getUserPassword();
+        String checkPassword = userRegisterRequest.getCheckPassword();
+        if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)) {
+            return null;
+        }
+        long result = userService.userRegister(userAccount, userPassword, checkPassword);
+        return ResultUtils.success(result);
+    }
+}
---
+++ src/main/java/com/benefit/service/impl/UserServiceImpl.java
@@ -0,0 +1,142 @@
+package com.benefit.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.benefit.exception.BusinessException;
+import com.benefit.mapper.UserMapper;
+import com.benefit.model.entity.User;
+import com.benefit.model.enums.ErrorCode;
+import com.benefit.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Allen
+ * @date 2025/6/6 15:39
+ */
+@Service
+@Slf4j
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+
+    @Resource
+    private UserMapper userMapper;
+
+    //盐值
+    private static final String SALT = "Allen";
+
+    /**
+     * 注册
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return
+     */
+    @Override
+    public long userRegister(String userAccount, String userPassword, String checkPassword) {
+        // 1. 校验
+        if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "参数为空");
+        }
+        if (userAccount.length() < 4) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户账号过短");
+        }
+        if (userPassword.length() < 8 || checkPassword.length() < 8) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户密码过短");
+        }
+        // 账户不能包含特殊字符
+        String validPattern = "[`~!@#$%^&*()+=|{}':;',\\\\[\\\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+        Matcher matcher = Pattern.compile(validPattern).matcher(userAccount);
+        if (matcher.find()) {
+            return -1;
+        }
+        // 密码和校验密码相同
+        if (!userPassword.equals(checkPassword)) {
+            return -1;
+        }
+        // 账户不能重复
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount", userAccount);
+        long count = userMapper.selectCount(queryWrapper);
+        if (count > 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "账号重复");
+        }
+        // 2. 加密
+        String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+        // 3. 插入数据
+        User user = new User();
+        user.setUserAccount(userAccount);
+        user.setUserPassword(encryptPassword);
+        boolean saveResult = this.save(user);
+        if (!saveResult) {
+            return -1;
+        }
+        return user.getId();
+    }
+
+    @Override
+    public User userLogin(String userAccount, String userPassword, HttpServletRequest request) {
+        return null;
+    }
+
+    @Override
+    public User getSafetyUser(User originUser) {
+        if (originUser == null) {
+            return null;
+        }
+        User safetyUser = new User();
+        safetyUser.setId(originUser.getId());
+        safetyUser.setUserAccount(originUser.getUserAccount());
+        safetyUser.setGender(originUser.getGender());
+        safetyUser.setPhone(originUser.getPhone());
+        safetyUser.setEmail(originUser.getEmail());
+        safetyUser.setUserRole(originUser.getUserRole());
+        safetyUser.setCreateTime(originUser.getCreateTime());
+        safetyUser.setUpdateTime(originUser.getUpdateTime());
+        safetyUser.setCreatedBy(originUser.getCreatedBy());
+        safetyUser.setUpdatedBy(originUser.getUpdatedBy());
+        return safetyUser;
+    }
+
+    @Override
+    public int userLogout(HttpServletRequest request) {
+        return 0;
+    }
+
+    @Override
+    public List<User> searchUsersByTags(List<String> tagNameList) {
+        return null;
+    }
+
+    @Override
+    public int updateUser(User user, User loginUser) {
+        return 0;
+    }
+
+    @Override
+    public User getLoginUser(HttpServletRequest request) {
+        return null;
+    }
+
+    @Override
+    public boolean isAdmin(HttpServletRequest request) {
+        return false;
+    }
+
+    @Override
+    public boolean isAdmin(User loginUser) {
+        return false;
+    }
+
+    @Override
+    public List<User> matchUsers(long num, User loginUser) {
+        return null;
+    }
+}
