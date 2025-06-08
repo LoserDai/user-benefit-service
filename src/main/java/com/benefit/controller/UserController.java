2335ce175d69f4ce99a78cc0cd801025ca346e02
Allen <15570237906@139.com>
2025-06-08 21:54:53 +0800

    fixed user/login

--- src/main/java/com/benefit/controller/UserController.java
+++ src/main/java/com/benefit/controller/UserController.java
@@ -31,7 +31,7 @@ import static com.benefit.constant.UserConstant.USER_LOGIN_STATUS;
 @RestController
 @RequestMapping("/user")
 //支持跨域请求后端
-@CrossOrigin(origins = {"http://localhost:3000"})
+@CrossOrigin(origins = {"http://localhost:8081"})
 @Api(tags = "用户操作入口")
 @Slf4j
 public class UserController {
@@ -92,6 +92,7 @@ public class UserController {
             return ResultUtils.error(ErrorCode.PARAMS_ERROR);
         }
         User user = userService.userLogin(account, password, request);
+        log.info("user login result:{}", "userName: " + user.getAccount());
         return ResultUtils.success(user);
     }
 
